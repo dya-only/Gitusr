343c28efba3380bd550aecb80b6a94aa2296cbed
dya-code <dyacode@proton.me>
2022-12-07 21:37:45 +0900

    followers & following deisgn

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,7 +2,7 @@
   import { push } from 'svelte-spa-router'
 </script>
 
-<nav class="navbar flex items-center fixed p-4 pl-8 bg-[#161B22] drop-shadow-2xl m-4 rounded-2xl">
+<nav class="navbar flex items-center fixed z-50 p-4 pl-8 bg-[#161B22] drop-shadow-2xl m-4 rounded-2xl">
   <a href="/"><img class="nav-logo mr-20 hover:brightness-50" src="/src/assets/svelte.svg" alt="" /></a>
   <a class="nav-item hover:text-white mr-10" href="/">Home</a>
   <a class="nav-item hover:text-white mr-10" href="https://github.com/dya-code" target="_blank" rel="noreferrer">Github</a>
--- src/lib/pages/User.svelte
+++ src/lib/pages/User.svelte
@@ -1,26 +1,23 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { beforeUpdate, onMount } from "svelte";
   // require("dotenv").config();
   import Fa from 'svelte-fa'
   import { faBuilding, faCompass, faLocationDot, faUser, faThumbTack } from '@fortawesome/free-solid-svg-icons'
 
   export let params
-
+  
   let res: any = {}
   let orgs: any = []
-  let flws: any = []
+  let _flws = []
+  let flws = []
+  let _flwn = []
+  let flwn = []
 
   onMount(async () => {
+
     const resp = await fetch(`https://api.github.com/users/${ params.id }`).then(resp => resp.json())
     const oresp = await fetch(`https://api.github.com/users/${ params.id }/orgs`).then(resp => resp.json())
-    
-    // if (oresp.length > 9) {
-    //   for (let i = 0; i < 9; i++) {
-    //     orgs[i] = oresp[i]
-    //   }
-    // } else {
-    //   orgs = oresp
-    // }
+
     orgs = oresp
     res = resp
     console.log(res)
@@ -36,22 +33,35 @@
       res.blog = "__"
     }
 
-    let p = Math.ceil(parseInt(res.followers) / 30)
+    let p:number = Math.ceil(parseInt(res.followers) / 30)
+    let p2:number = Math.ceil(parseInt(res.following) / 30)
 
     for (let i = 0; i < p; i++) {
       const followers = await fetch(`https://api.github.com/users/${ params.id }/followers?page=${ i + 1 }`).then(resp => resp.json())
 
-      followers.forEach(el => {
-        flws.push({ name: el.login, avatar: el.avatar_url })
+      followers.forEach((el: any) => {
+        _flws.push(el)
       })
+
     }
-    console.log(flws)
+    flws = _flws
+
+    for (let i = 0; i < p2; i++) {
+      const following = await fetch(`https://api.github.com/users/${ params.id }/following?page=${ i + 1 }`).then(resp => resp.json())
+
+      following.forEach((el: any) => {
+        _flwn.push(el)
+      })
+
+    }
+    flwn = _flwn
+
+    console.log(flwn)
   })
 
 </script>
 
-<main>
-
+<main class="overflow-hidden">
   <div class="user h-screen w-screen flex justify-center items-center flex-col">
     <div class="flex justify-center items-end flex-wrap">
   
@@ -98,8 +108,8 @@
       <!-- Organizations -->
       <div class="org-card p-10 mb-5 mr-5 rounded-l-2xl rounded-r-2xl drop-shadow-2xl bg-[#161B22] max-h-[150px] overflow-auto scrollbar scrollbar-thumb-neutral-700 scrollbar-thumb-rounded-full scrollbar-thin flex justify-center items-center flex-wrap max-w-[400px]">
         { #each orgs as org }
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <img class="org-img m-2 rounded-2xl w-12 cursor-pointer transition ease-out hover:brightness-50" src={ org.avatar_url } alt="" on:click={ () => { window.open(`http://github.com/${org.login}`, '_blank') } } />
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <img class="org-img m-2 rounded-2xl w-12 cursor-pointer transition ease-out hover:brightness-50" src={ org.avatar_url } alt="" on:click={ () => { window.open(`http://github.com/${org.login}`, '_blank') } } />
         { /each }
   
         <!-- <div class="org-more m-2 rounded-2xl w-12 h-12 cursor-pointer transition ease-out hover:brightness-50 bg-[#161B22] text-4xl flex justify-center" on:click={ () => { window.open(`http://github.com/${res.login}`, '_blank') } }>+</div> -->
@@ -110,22 +120,41 @@
         <Fa class="mr-4" icon={faThumbTack} fw size="lg" />
         <div class="text">{ res.bio }</div>
       </div>
-  
     </div>
+
   </div>
 
-  <div class="under w-screen flex justify-center items-center flex-col">
-    <!-- Followers -->
-    <div class="flwrs-card p-10 rounded-l-2xl rounded-r-2xl drop-shadow-2xl bg-[#161B22] max-h-[500px] overflow-auto scrollbar scrollbar-thumb-neutral-700 scrollbar-thumb-rounded-full scrollbar-thin flex justify-center items-center flex-wrap max-w-[400px]">
-      { #each flws as flw }
-      <!-- svelte-ignore a11y-click-events-have-key-events -->
-      <img class="flw-img m-2 rounded-2xl w-12 cursor-pointer transition ease-out hover:brightness-50" src={ flw.avatar } alt="" on:click={ () => { window.open(`http://github.com/${flw.name}`, '_blank') } } />
-      { /each }
-  
-    </div>
-  </div>
+  <div class="under h-screen w-screen flex justify-center items-center flex-col">
 
-  
+    <div class="flex justify-center items-end">
+
+      <!-- Followers -->
+      <div class="flw-card p-10 mb-5 mr-5 rounded-l-2xl rounded-r-2xl drop-shadow-2xl bg-[#161B22] max-h-[300px] w-[600px] overflow-auto scrollbar scrollbar-thumb-neutral-700 scrollbar-thumb-rounded-full scrollbar-thin flex justify-start items-start flex-col">
+        <span class="font-bold text-xl mb-8">Followers</span>
+        { #each flws as flw }
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <div class="flex justify-start items-center transition ease-in-out hover:brightness-50 hover:0 cursor-pointer" on:click={ () => { window.open(`http://github.com/${flw.login}`, '_blank') } }>
+            <img class="flw-img m-2 rounded-2xl w-12" src={ flw.avatar_url } alt="" />
+            <div class="flw-name font-bold">{ flw.login }</div>
+          </div>
+        { /each }
+      </div>
+
+      <!-- Following -->
+      <div class="flw-card p-10 mb-5 mr-5 rounded-l-2xl rounded-r-2xl drop-shadow-2xl bg-[#161B22] max-h-[300px] w-[600px] overflow-auto scrollbar scrollbar-thumb-neutral-700 scrollbar-thumb-rounded-full scrollbar-thin flex justify-start items-start flex-col">
+        <span class="font-bold text-xl mb-8">Following</span>
+        { #each flwn as fln }
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <div class="flex justify-start items-center transition ease-in-out hover:brightness-50 hover:0 cursor-pointer" on:click={ () => { window.open(`http://github.com/${fln.login}`, '_blank') } }>
+            <img class="flw-img m-2 rounded-2xl w-12" src={ fln.avatar_url } alt="" />
+            <div class="flw-name font-bold">{ fln.login }</div>
+          </div>
+        { /each }
+      </div>
+
+    </div>
+
+  </div>
 </main>
 
 <style>
