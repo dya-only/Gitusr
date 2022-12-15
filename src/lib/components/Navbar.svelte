d3588ede19ee26701f74c02aed834bca8068d2e3
dya-code <entrydia7691@gmail.com>
2022-12-15 13:32:09 +0900

    change navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <nav class="navbar flex items-center fixed w-[95%] z-50 pl-8 p-2 bg-[#161B22] drop-shadow-2xl rounded-b-xl">
-  <a href="/"><img class="nav-logo w-5 mr-12 hover:brightness-50" src="/src/assets/svelte.svg" alt="" /></a>
+  <!-- <a href="/"><img class="nav-logo w-5 mr-12 hover:brightness-50" src="/src/assets/svelte.svg" alt="" /></a> -->
   <a class="nav-item hover:text-white mr-10" href="/">Home</a>
   <a class="nav-item hover:text-white mr-10" href="https://github.com/dya-code" target="_blank" rel="noreferrer">Github</a>
   <a class="nav-item hover:text-white mr-10" href="https://discord.com/users/884954874943520788" target="_blank" rel="noreferrer">Discord</a>
